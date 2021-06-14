5e2666f07f0e2d4ee38adbe7068e9f10a3f6f340
Ha Truong <td.xuha@gmail.com>
2021-06-14 19:54:42 +0700

    Add Refresh button

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -8,6 +8,11 @@ function App() {
   const [tours, setTours] = useState([]);
   const [loading, setLoading] = useState(true);
 
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
   const getTours = async () => {
     try {
       const response = await fetch(url);
@@ -24,10 +29,26 @@ function App() {
     getTours();
   }, []);
 
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button className="btn" onClick={() => getTours()}>
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+
   return (
     <main>
-      {loading && <Loading />}
-      <Tours tours={tours} />
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   );
 }
